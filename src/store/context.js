fca3f7ba81d2e5d4f6e0830a3bee593672e7e026
Theodorus Jossoey <jossoey84@gmail.com>
2022-03-16 16:12:23 -0700

    Update context.js
    Making sure that fetching the API will return the right list of object that can be used later on for displaying.
    
    Encounter an error "Unexpected token < in JSON at position 0"
    
    Fix: adding https:// into the searchByName and searchRandom url

--- src/store/context.js
+++ src/store/context.js
@@ -1,8 +1,8 @@
 import { useState, useContext, createContext, useEffect } from "react";
 import { useCallback } from "react";
 
-const searchByName = "www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-const searchRandom = "www.thecocktaildb.com/api/json/v1/1/random.php";
+const searchByName = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+// const searchRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
 const AppContext = createContext();
 
@@ -10,6 +10,43 @@ export function AppProvider({ children }) {
   const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("a");
   const [loadedCocktails, setLoadedCocktails] = useState([]);
+
+  const fetchCocktail = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${searchByName}${searchTerm}`);
+      const data = await response.json();
+      console.log(response);
+      const { drinks } = data;
+      if (drinks !== null) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass, strInstructions } =
+            item;
+
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+            instruction: strInstructions,
+          };
+        });
+        setLoadedCocktails(newCocktails);
+        console.log(newCocktails);
+      } else {
+        setLoadedCocktails([]);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchCocktail();
+  }, [searchTerm, fetchCocktail]);
 
   return (
     <AppContext.Provider value={{ isLoading, loadedCocktails, setSearchTerm }}>
